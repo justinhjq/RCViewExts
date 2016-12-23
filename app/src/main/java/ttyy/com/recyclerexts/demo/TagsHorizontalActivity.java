777b9cb88f88618eac31ac3f84bbe8b7ecce92b2
justinhjq <173342716@qq.com>
2016-12-23 09:37:06 +0800

    Add files via upload

---
+++ app/src/main/java/ttyy/com/recyclerexts/demo/TagsHorizontalActivity.java
@@ -0,0 +1,42 @@
+package ttyy.com.recyclerexts.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import ttyy.com.recyclerexts.base.EXTRecyclerAdapter;
+import ttyy.com.recyclerexts.base.EXTViewHolder;
+import ttyy.com.recyclerexts.tags.TagsHorizontalLayoutManager;
+import ttyy.com.recyclerexts.tags.TagsVerticalLayoutManager;
+
+public class TagsHorizontalActivity extends AppCompatActivity {
+
+    RecyclerView recycler_view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tags_horizontal);
+
+        recycler_view = (RecyclerView) findViewById(R.id.recycler_view);
+
+        recycler_view.setLayoutManager(new TagsHorizontalLayoutManager());
+
+        EXTRecyclerAdapter<String> adapter = new EXTRecyclerAdapter<String>(R.layout.item_tag_view) {
+            @Override
+            public void onBindViewHolder(EXTViewHolder holder, int position, String data) {
+                holder.setText(R.id.text, data);
+            }
+        };
+
+        ArrayList<String> datas = new ArrayList<>();
+        Collections.addAll(datas, getResources().getStringArray(R.array.menu));
+        adapter.setDatas(datas);
+
+        recycler_view.setAdapter(adapter);
+
+    }
+}
