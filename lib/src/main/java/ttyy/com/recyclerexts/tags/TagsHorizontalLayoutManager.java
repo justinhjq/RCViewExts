9545b7b9d19d97e89c0d2b013d2a1b9d79de24ae
justinhjq <173342716@qq.com>
2016-12-23 09:40:56 +0800

    Add files via upload

---
+++ lib/src/main/java/ttyy/com/recyclerexts/tags/TagsHorizontalLayoutManager.java
@@ -0,0 +1,20 @@
+package ttyy.com.recyclerexts.tags;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Author: Administrator
+ * Date  : 2016/12/23 09:31
+ * Name  : TagsHorizontalLayoutManager
+ * Intro : Edit By Administrator
+ * Modification  History:
+ * Date          Author        	 Version          Description
+ * ----------------------------------------------------------
+ * 2016/12/23    Administrator   1.0              1.0
+ */
+public class TagsHorizontalLayoutManager extends RecyclerView.LayoutManager {
+    @Override
+    public RecyclerView.LayoutParams generateDefaultLayoutParams() {
+        return null;
+    }
+}
