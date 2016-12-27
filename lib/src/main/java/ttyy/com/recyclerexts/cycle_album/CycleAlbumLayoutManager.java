8fe73bb2d0383684d55e6a694a2ca71d45926af5
justinhjq <173342716@qq.com>
2016-12-27 16:44:49 +0800

    Add files via upload

---
+++ lib/src/main/java/ttyy/com/recyclerexts/cycle_album/CycleAlbumLayoutManager.java
@@ -0,0 +1,20 @@
+package ttyy.com.recyclerexts.cycle_album;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Author: Administrator
+ * Date  : 2016/12/27 13:52
+ * Name  : CycleAlbumLayoutManager
+ * Intro : 循环相册 布局
+ * Modification  History:
+ * Date          Author        	 Version          Description
+ * ----------------------------------------------------------
+ * 2016/12/27    Administrator   1.0              1.0
+ */
+public class CycleAlbumLayoutManager extends RecyclerView.LayoutManager {
+    @Override
+    public RecyclerView.LayoutParams generateDefaultLayoutParams() {
+        return null;
+    }
+}
